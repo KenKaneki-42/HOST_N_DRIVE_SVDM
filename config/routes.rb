75ad70a71f121264702fe463f1e77232f6cdc633
Kenkaneki-42 <sylvain.vandermeersch@gmail.com>
2022-09-15 14:18:51 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :listings, only: %i[index show create] do
+    resources :reservations, only: %i[create destroy index show]
+    resources :bookings, only: %i[create destroy index show]
+    resources :missions, only: %i[create index show]
+  end
 end
