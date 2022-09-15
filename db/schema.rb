e6916ae9c2574d5c1e2293bd538c326ee2adb52a
Kenkaneki-42 <sylvain.vandermeersch@gmail.com>
2022-09-15 12:50:13 +0200

    add model mission

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_104744) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_104951) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_104744) do
     t.integer "num_room"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "missions", force: :cascade do |t|
+    t.string "mission_type"
+    t.string "price"
+    t.bigint "listing_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_missions_on_listing_id"
   end
 
   create_table "reservations", force: :cascade do |t|
@@ -51,5 +60,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_104744) do
   end
 
   add_foreign_key "bookings", "listings"
+  add_foreign_key "missions", "listings"
   add_foreign_key "reservations", "listings"
 end
